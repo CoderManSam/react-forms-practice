62cd10dc3aa1de2a3d9b6aab94600f7dd9a00a7a
Samuel Thomas <samuel.thomas93@ntlworld.com>
2022-07-21 13:05:57 +0100

    adding completed exercise

--- src/App.js
+++ src/App.js
@@ -4,28 +4,70 @@ import "./App.css";
 export default function App() {
   
   //TODO: Add your state fields here
+  const initialFormState = {
+    name: "",
+    address: "", 
+    phone: "",
+    email: "",
+    complaint: "",
+    contact: "",
+    consent: false
+  }
+
+  const [userDetails, setUserDetails] = useState(initialFormState)
+
+  const updateUserDetails = (event) => {
+    event.preventDefault()
+
+    const eventName = event.target.name
+    const eventValue = event.target.value
+
+    const newUserDetails = {...userDetails, [eventName]: eventValue}
+
+    setUserDetails(newUserDetails)
+
+    console.log("state name", eventName)
+
+    console.log("state value ", eventValue)
+  }
+
+  const submitForm = (event) => {
+    event.preventDefault()
+
+    fetch(URL, {
+      method: "POST",
+
+      body: JSON.stringify(userDetails)
+    })
+
+    setUserDetails(initialFormState)
+  }
+
+  const displayState = () => {
+    console.log("state is ", userDetails)
+  }
 
   return (
     <>
-      <form className="form">
+      <form className="form" onSubmit={(submitForm, displayState)}>
         <h2>Complaining form!</h2>
         <div className="form__section-left">
           <label>
             Full name
-            <input type="text" name="name" required />
+            <input onChange={updateUserDetails} value={userDetails.name} type="text" name="name" required />
           </label>
           <label>
             Address
-            <input type="text" name="address" />
+            <input onChange={updateUserDetails} value={userDetails.address} type="text" name="address" />
           </label>
           <label>
             Phone Number
-            <input type="tel" name="phone" />
+            <input onChange={updateUserDetails} value={userDetails.phone} type="tel" name="phone" />
           </label>
 
           <label>
             Email
-            <input type="email" name="email" />
+            <input onChange={updateUserDetails} value={userDetails.email} type="email" name="email" />
           </label>
         </div>
 
@@ -33,38 +75,40 @@ export default function App() {
           <label>
             Write your complaint
             <textarea
+              onChange={updateUserDetails} 
+              value={userDetails.complaint}
               name="complaint"
               rows="10"
               placeholder="You can complain here"
             ></textarea>
           </label>
-
+          {/* BUG HERE RADIO ISNT BEING TICKED AND IT IS WHEN CHECKBOX BELOW IS */}
           <div className="form__radio-group">
             <p>How do you want to be contacted? </p>
             <label>
-              <input type="radio" name="contact" value="phone" />
+              <input onChange={updateUserDetails} checked={userDetails.contact === "phone"} type="radio" name="contact" value="phone" />
               Phone
             </label>
 
             <label>
-              <input type="radio" name="contact" value="email" />
+              <input onChange={updateUserDetails} checked={userDetails.contact === "email"} type="radio" name="contact" value="email" />
               Email
             </label>
 
             <label>
-              <input type="radio" name="contact" value="post" />
+              <input onChange={updateUserDetails} checked={userDetails.contact === "post"} type="radio" name="contact" value="post" />
               Slow Mail
             </label>
 
             <label>
-              <input type="radio" name="contact" value="none" />
+              <input onChange={updateUserDetails} checked={userDetails.contact === "none"} type="radio" name="contact" value="none" />
               No contact!
             </label>
           </div>
 
           <label>
             I agree you take my data, and do whatever
-            <input type="checkbox" name="consent" id="consent" />
+            <input onChange={updateUserDetails} checked={userDetails.consent} type="checkbox" name="consent" id="consent" value="checked" />
           </label>
         </div>
         <input type="submit" value="Submit!" />
